5ea4ca2b788d3731b6ece84d14cc6cc88a2c8f91
Henry-GongZY <watershell@qq.com>
2022-10-27 15:02:05 +0800

    Do some modifications

--- proj_03_06.m
+++ proj_03_06.m
@@ -7,13 +7,13 @@ imshow(img);
 img = double(img);
 w = fspecial('average',3);
 img = double(img);
-c=conv2(img,w,'same');
+c = conv2(img,w,'same');
 g = img - c;
 figure(2);
 imshow(g);
  
 A=[0.6,1,1.7,2.3,2.9,5,10];
-for i=1:7
+for i = 1:7
 d = img + A(i) * c;
 d = uint8(d);
 figure(i+2);
